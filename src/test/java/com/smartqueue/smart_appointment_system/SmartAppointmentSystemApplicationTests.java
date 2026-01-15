bbad6cd0be3b57cc1cdb266cb3d85120ca25157e
Anil <anilsahu9786@gmail.com>
2026-01-15 23:01:04 +0530

    Initial commit

---
+++ src/test/java/com/smartqueue/smart_appointment_system/SmartAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartqueue.smart_appointment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
